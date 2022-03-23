3338a2e28e4632b552c6cbcfc77b0edbf9725e98
Kia King Ishii <kia.king.08@gmail.com>
2022-03-23 20:49:37 +0900

    feat: add favicon (#1)
    close #1

--- src/.vitepress/config.ts
+++ src/.vitepress/config.ts
@@ -1,9 +1,10 @@
 export default {
-lang: 'ja-JP',
+  lang: 'ja-JP',
   title: 'POYO - FF XIV FC | Yes, we’re hardcore',
   description: '困難を克服し、絶望を打ち壊し、未踏の地を踏み締める。ぼくたちは、ハードコアの血を流す。',
 
   head: [
+    ['link', { rel: 'icon', href: '/favicon-32.png', sizes: "32x32" }],
     ['link', { rel: 'preconnect', href: 'https://fonts.googleapis.com' }],
     ['link', { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true }],
     ['link', { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&family=Noto+Sans+JP:wght@400;500;700&display=swap' }]
